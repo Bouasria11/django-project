449d5936432b1cf0a710499f06f3814d0c6cc43c
Bouasria11 <yassine.bouasria11@gmail.com>
2026-04-23 10:00:00 +0100

    Backdated update: film_platform/urls.py

--- film_platform/urls.py
+++ film_platform/urls.py
@@ -1,4 +1,4 @@
-"""film_platform URL Configuration"""
+"""Configuration des URLs principales du projet film_platform."""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -6,11 +6,13 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Routes de l'application web movies.
     path('', include('movies.urls')),
-    # API endpoints
+    # Routes de l'API REST.
     path('api/', include('movies.api_urls')),
 ]
 
 if settings.DEBUG:
+    # Sert les fichiers medias et statiques uniquement en developpement.
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
